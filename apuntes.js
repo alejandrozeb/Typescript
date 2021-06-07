32a06beb4fdd948b7d6f3f4cc38fe8313ed226d8
unknown <alejandrojzeballos@gmail.com>
2021-06-07 18:23:08 -0400

    {add} instalando typescript como dependencia

--- apuntes.js
+++ apuntes.js
@@ -7,4 +7,16 @@
     tenemos sistema de tipos o tipado ademas depedndiendo del id vamos identificando que errores surgen en funciones o metodos.
 
     Ademas TS es transpilado (o copilado, el transpilado es cuando va directo a maquina) con babel o tscopiler esto nos permite transpilar a js con emac3 hasta el que queramos.
+    herramientas
+    node 12.18.3
+    npm 6.14.6
+
+    podemos instalar typescipt de manera global pero al momento de desarrollar en equipo se debe manejar el la version que queramos.
+
+    instalando typescript como depdendencia
+    npm init
+    npm i --save-dev typescript
+
+    en typescript usaremos la version 4
+    tsc es un copilador de typpescript
 */
