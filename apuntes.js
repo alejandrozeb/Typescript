0262fe496c672694b361f5feaeae37f030bd8863
unknown <alejandrojzeballos@gmail.com>
2021-06-07 18:12:33 -0400

    {add} definicion de typescript

--- apuntes.js
+++ apuntes.js
@@ -1,1 +1,10 @@
-/* apuntes curso tipescript */
+/* apuntes curso tipescript 
+    typescript es un superconjunto de js
+    es JS con mas cosas. TS  incorpora hergonomia el trabajo es mas facil, mas facil de debbugear
+    seguridad frente a errores de tipo que se tiene en js.
+    errores surgen en js al momento del deploy.
+    
+    tenemos sistema de tipos o tipado ademas depedndiendo del id vamos identificando que errores surgen en funciones o metodos.
+
+    Ademas TS es transpilado (o copilado, el transpilado es cuando va directo a maquina) con babel o tscopiler esto nos permite transpilar a js con emac3 hasta el que queramos.
+*/
