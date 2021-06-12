a34bfcc70de8ba6e98aa5efdb9606c7bbc97262c
unknown <alejandrojzeballos@gmail.com>
2021-06-12 00:46:33 -0400

    {add} atributos private

--- 02-clases/clases/01-clases.ts
+++ 02-clases/clases/01-clases.ts
@@ -1,9 +1,11 @@
 //sintaxis 
 
 class Rectangulo{
-    ancho: number;
-    alto:number;
+    private ancho: number;
+    private alto:number;
     //debemostener un contructor o no vamos a poder instanciar un objeto
+    //otro metodo de declara una variable privada, es proximas versiones en js ya no se podra acceder
+    //#area: number;
 
     constructor(ancho, alto){
         this.ancho = ancho;
@@ -25,3 +27,6 @@ let r1 = new Rectangulo(4,3);
 let r2 = new Rectangulo(5,3);
 console.log(r1.area());
 console.log(r2.perimetro());
+
+
+//con private podemos limitar el accesso a esos atributos, metodos, funciones a solo la clase y ano desde afuera.
