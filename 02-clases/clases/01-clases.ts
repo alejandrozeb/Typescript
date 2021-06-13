e4c10d3fbb8b0388090ea2a2637c76f11bf5d0b0
unknown <alejandrojzeballos@gmail.com>
2021-06-13 00:11:44 -0400

    {add} getters y setters en ts

--- 02-clases/clases/01-clases.ts
+++ 02-clases/clases/01-clases.ts
@@ -6,13 +6,14 @@ class Rectangulo{
     //debemostener un contructor o no vamos a poder instanciar un objeto
     //otro metodo de declara una variable privada, es proximas versiones en js ya no se podra acceder
     //#area: number;
+    private _nombre: string = " ";
 
     constructor(ancho: number, alto: number){
         this.ancho = ancho;
         this.alto = alto;
     }
 
-    area(){
+    get area(){ //es una funcion por dentro pero por fuera es un atributo
         return this.ancho * this.alto;
     }
 
@@ -20,6 +21,13 @@ class Rectangulo{
         return this.ancho * 2 + this.alto * 2;
     }
 
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(value: string){
+        this._nombre = value;
+    }
 
 }
 //se tipa para que pueda reconocer los atributos del objeto
@@ -27,10 +35,14 @@ class Rectangulo{
 
 let r1 = new Rectangulo(4,3);
 let r2 = new Rectangulo(5,3);
-console.log(r1.area());
+console.log(r1.area);
 console.log(r2.perimetro());
+r1.nombre = "A";
 
+//personalmento no recomiendo el uso de get y set en objetos solo en estrucutras de datos para profindurizar revisar clean code el capitulo de clases
 
 //con private podemos limitar el accesso a esos atributos, metodos, funciones a solo la clase y ano desde afuera.
 
 //readonly ts vigila si se hace un cambio en dicho atributo o funcion en java es conocido como final, no se debe mezclar con private o podemos crear atributos que nose modifican ni el propia clase
+
+//podemos usar getters y setters es una funcion que devuelve un atributo, podemos pedir su valor y establecerlo
