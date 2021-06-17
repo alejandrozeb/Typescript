6380d734a6622a7f36817b2b98ada9d8eb11933e
unknown <alejandrojzeballos@gmail.com>
2021-06-17 01:12:28 -0400

    {add} genericos multiples

--- 06-genericos/genericos/01-genericos.ts
+++ 06-genericos/genericos/01-genericos.ts
@@ -1,22 +1,27 @@
 //genericos es una fomra de meter parametros con definiciones de tipos
 
-interface Respuesta<T>{
+interface Respuesta<Mensaje, Extra>{
     type:string,
     success: boolean,
-    message: T
+    message: Mensaje,
+    whatever: Extra
 }
 //T puede ser booblean, string pero debes dar de que tipo sera
 
-let payload: Respuesta<string> = {
+let payload: Respuesta<string, number> = {
     type: 'post',
     success: true,
-    message: 'sadasdasd'  
+    message: 'sadasdasd',
+    whatever: 5
 }
 //indicamos que el generico debe ser de tpo string
 
-let payload2: Respuesta<boolean> = {
+let payload2: Respuesta<boolean, string> = {
     type: 'post',
     success: true,
-    message: true
+    message: true,
+    whatever: "cualquiercosa"
 }
 //los genericos buscan influenciar los tipos internos de una clases,interfaces, funciones
+
+//en los genericos siempre usan letras a diferencia de otros lenguajes pueden tener mas letras
