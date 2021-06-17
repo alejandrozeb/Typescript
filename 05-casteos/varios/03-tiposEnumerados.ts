52d67bd011e4171d684b3d609210a1f1225ba53c
unknown <alejandrojzeballos@gmail.com>
2021-06-16 23:56:32 -0400

    {add} enumerados avanzado

--- 05-casteos/varios/03-tiposEnumerados.ts
+++ 05-casteos/varios/03-tiposEnumerados.ts
@@ -1,8 +1,16 @@
 //tipos enumerados es un tipo novedoso no exite en js simula codigo para que funcione en nodejs
 //el proposito es limitar el rango de una variable
 
-enum diasSemana{lunes, martes, miercoles, jueves, viernes, sabado, domingo};
-
+enum diasSemana{
+    lunes = 401, 
+    martes, 
+    miercoles, 
+    jueves = 701, 
+    viernes, 
+    sabado, 
+    domingo
+};
+//se reasigna el index tabien pueden tener el mismo valor de indexado pero no es recomendable
 interface CitaMedica{
     dia: diasSemana;
 }
@@ -11,3 +19,21 @@ interface CitaMedica{
 let p: CitaMedica = {
     dia: diasSemana.domingo
 }
+
+//cuando declaras un tipo numeros el sistema de tipos crea una serie de indices por el orden de declaracion del rango de valores, pero tambien podemos asignar un valor y los siguinentes seguiran
+
+console.log(p);
+//imprime el index no el valor
+
+
+enum diasSemana2{
+    lunes = "lunes", 
+    martes = "Martes", 
+    miercoles = "Miercoles", 
+    jueves = 6,//indexa 
+    viernes, 
+    sabado, 
+    domingo
+};
+
+//debemos declarar  las constantes como literales 
