4be601fb0fc6bcb652855261075ac81b23d4d1ff
unknown <alejandrojzeballos@gmail.com>
2021-06-19 01:49:22 -0400

    {add} export default

--- 07-modulos/module/facturas.ts
+++ 07-modulos/module/facturas.ts
@@ -1,6 +1,7 @@
 //IMPORTANdo un producto
 import { Producto } from "./almacen";
-
+import IVA, {Precio} from "./impuesto";
+//import * as impustos form "./impuesto.ts extrae todos los export de el archivo" 
 function procesarPedido(p: Producto){
     console.log('procesando...');
 }
---
+++ 07-modulos/module/impuesto.ts
@@ -0,0 +1,8 @@
+export default function IVA(precio:number){
+    return precio * 1.21;
+}
+//podemos exportar esot en especifico
+
+//podemos juntar export defaul y export normal
+
+export type Precio = number;
