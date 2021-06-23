9fe9c00820536fe303054c77c4fab203fdffd539
unknown <alejandrojzeballos@gmail.com>
2021-06-23 00:13:56 -0400

    {add} agregando el json

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,24 @@
+import { match } from 'assert';
 import express from 'express';
+import { getClub, getResultado } from './database';
 
 export default function(){
     const app = express()
 
     app.get("/resultados/:equipo1/:equipo2",(req, res)=>{
-        res.send(`resultados del ${req.params.equipo1} vs ${req.params.equipo2}`);
+        try{
+            let team1 = getClub(req.params.equipo1);
+            let team2 = getClub(req.params.equipo2);
+            res.json({
+                team1, team2
+            })
+        } catch(e){
+            res.send(400).json({
+                error:e
+            })
+        }
+
+
     });
     return app
 }
